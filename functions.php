26ca5ab478e3749ae44c5ca4ee325feec99a3412
rodica-andronache <rodik_andronache@yahoo.com>
2015-08-18 14:03:46 +0300

    Updated not found page, remove style included twice and other small fixes

--- functions.php
+++ functions.php
@@ -1,15 +1,9 @@
 <?php
 
-// add any new or customised functions here
-
 add_action( 'wp_enqueue_scripts', 'zblackbeard_enqueue_styles' );
+
 function zblackbeard_enqueue_styles() {
-
-    	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('zerif_bootstrap_style') );
-
-	// Loads our main stylesheet.
-	wp_enqueue_style( 'zerif-child-style', get_stylesheet_uri(), array('zerif_style'), 'v1' );
-
+	wp_enqueue_style( 'zblackbeard-style', get_template_directory_uri() . '/style.css', array('zerif_bootstrap_style') );
 }
 
 function zblackbeard_remove_style_child(){
